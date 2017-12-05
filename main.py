caa3ee3be2df580e6c4a207af1fc1ee24ae4858a
pholec <pholec@housebirnbaum.mit.edu>
2017-12-05 17:54:34 -0500

    completed main build, but running very slow

--- main.py
+++ main.py
@@ -9,55 +9,109 @@ Tests out the new and improved variable solver
 
 # homegrown libraries
 from solver.methods import match_probability
-from datasim.seq_generator import *
+from sequence_generator import *
 
 #------------------------------------------------------------------------------# 
 
-def main():
-    sg = SequencingGenerator()
-    sg.set_cells_per_well(distro_type = 'explicit', cells_per_well = cpw_by_well)
-    sg.set_options(**self.options)
-    data = sg.generate_data()
-
 def main(*args,**kwargs):
 
-    print 'Starting optimization procedure...'
-
-
-    '''
-    Options: 
-        > num_wells
-        > cells_per_well_distibution
-        > num_cells
-        > cells
-        > cell_frequency_distribution
-        > chain_misplacement_prob
-        > chain_deletion_prob
-        > alpha_sharing_probs
-        > beta_sharing_probs
-    '''
-
     options = {
-                'num_cells':100,
-                'chain_deletion_prob':0.1,
-                'alpha_sharing_probs':0.0,
-                'beta_sharing_probs':0.0
+              'num_wells':[96],
+              'cpw':[100],
+              'num_cells':100,
+              'cell_freq_distro':'power-law',
+              'cell_freq_constant':-1,
+              'chain_misplacement_prob':0,
+              'chain_deletion_prob':0.1
               }
 
-    # Convert well partitions to iterperable
-    cpw_distro = [(p,w) for p,w in zip(parameters,self.options['well_partitions'])]
-    cpw_by_well = [wt[0] for wt in cpw_distro for i in xrange(wt[1])]
-    
-    # Sequence Generator
-    sg = SequencingGenerator()
-    sg.set_options(**options)
-    sg.set_cells_per_well(distro_type = 'explicit', cells_per_well = cpw_by_well)
+    # Update settings
+    for arg in args: options.update(arg)
+    options.update(kwargs)
+
+    # Pull out values into local namespace
+    num_wells = options['num_wells']
+    cpw = options['cpw']
+    threshold = 0.01
+    prior_alpha = 1
+
+    # Generate datasets
+    sg = DataGenerator(options)
+    sg.generate_cells()
     data = sg.generate_data()
 
-    # generate results from solver, from data
-    results = solve_madhype(data,pair_threshold=2.0)
+    # Identify unique a,b sequences
+    uniques = {
+               'A':frozenset().union(*data['well_data']['A']),
+               'B':frozenset().union(*data['well_data']['B'])
+              }
+   
+    # Initialize well distribution
+    well_distribution = {'A':{},'B':{}}
+
+    # Create range markers for number of wells 
+    pts = [0]+[sum(num_wells[:i]) for i in xrange(len(num_wells))]
+    
+    # Identifying well placements for each unique chain
+    for label in ('A','B'):
+        print 'Starting {} chain identification...'.format(label)
+        for index in uniques[label]:
+            well_distribution[label][index] = \
+                    [set([w for w in xrange(pts[i],pts[i+1]) if index in data['well_data'][label][w]])
+                        for i in xrange(len(num_wells))]
+        print 'Finished {} chain identification.'.format(label)
+
+    # TODO: include frequency filter 
+    # TODO: standardize threshold
+
+    results = [] # initialize list
+
+    # Iterate through combinations!
+    for i,a in enumerate(uniques['A']):
+        if i % 1 == 0: print 'Starting A-chain {}...'.format(i)
+        for j,b in enumerate(uniques['B']):
+            # set up input
+            pair_data = _data_intersect(
+                    well_distribution['A'][a],well_distribution['B'][b],cpw)
+            pair_data['alpha'] = prior_alpha
+            pair_data['cpw'] = cpw
+            
+            # calculate match probability
+            p = match_probability(pair_data)
+            
+            if p > 5:
+                results.append((((a,),(b,)),p))
+
+    results.sort(key=lambda x: x[1])
+
+    print results
+
+    '''
+    print well_distribution['A'][0]
+    print well_distribution['B'][0]
+    print data['cells'][((0,),(0,))]
+    '''
 
 
+#------------------------------------------------------------------------------# 
+""" Internal Methods """
+#------------------------------------------------------------------------------# 
+
+def _data_intersect(d1,d2,num_wells):
+    """ Inputs two lists of sets of indices, formats for match_probability """
+    w_ij = tuple(len(s1.intersection(s2)) for s1,s2 in zip(d1,d2))
+    w_i  = tuple(len(s1) - w for s1,w in zip(d1,w_ij))
+    w_j  = tuple(len(s2) - w for s2,w in zip(d2,w_ij))
+    w_o  = tuple(w4 - w2 - w3 + w4 for w1,w2,w3,w4 in zip(w_ij,w_i,w_j,num_wells))
+    return {
+            'w_i':w_i,
+            'w_j':w_j,
+            'w_ij':w_ij,
+            'w_o':w_o,
+            'w_tot':num_wells
+           }
+    
+    
 
 #------------------------------------------------------------------------------# 
 
--- sequence_generator.py
+++ sequence_generator.py
@@ -12,27 +12,33 @@ This is a new sequence generator that is compatible with variable cpw
 
 """
 
+# standard libraries
+import itertools
+
+# nonstandard libraries
+import numpy as np
+from numpy.random import binomial
+
+# homegrown libraries
+
 #------------------------------------------------------------------------------# 
 
 class DataGenerator(object):
 
-    # I don't know if I wanna keep this stuff
-    """ Encapsulates parameters for generating sequencing data from a simulated experiment.
-    Accepts the following parameters:
-    Number of wells
-    - num_wells [READ/WRITE]
-    Distribution of cells/well
-    - cells_per_well_distribution [READ ONLY]
-    - cells_per_well_distribution_params [READ ONLY]
-    - set_cells_per_well()
-    Cell information
-    - cells [READ/WRITE]
-    Cell frequency distribution
-    - cell_frequency_distribution [READ ONLY]
-    - cell_frequency_distribution_params [READ ONLY]
-    - set_cell_frequency_distribution()
-    Noise stuff
-    - chain_misplacement_prob [READ/WRITE]
+    """
+    Methods:
+        __init__(self,*args,**kwargs)
+            input: dicts, kwargs
+            output: (none)
+        update(self,*args,**kwargs)
+            input: dicts, kwargs
+            output: (none)
+        generate_cells(self)
+            input: (none)
+            output: (none)
+        generate_data(self)
+            input: (none)
+            output: data dict 
     """ 
 
     def __init__(self, *args, **kwargs):
@@ -42,13 +48,12 @@ class DataGenerator(object):
         ## Set default parameter values
         self.settings = {
                         'num_wells':96,
-                        'cpw_distro':'constant',
                         'cpw':35,
                         'num_cells':1000,
                         'cell_freq_distro':'power-law',
                         'cell_freq_constant':-1,
                         'chain_misplacement_prob':0,
-                        'chain_deletion_prob':0
+                        'chain_deletion_prob':0.1
                         }
 
         self.cells = None
@@ -56,7 +61,7 @@ class DataGenerator(object):
         # update settings 
         self.update(*args,**kwargs)
 
-    def update(*args,**kwargs):
+    def update(self,*args,**kwargs):
 
         """ Update settings using *args and **kwargs """ 
 
@@ -68,19 +73,19 @@ class DataGenerator(object):
         """ Generate cells from user settings"""
 
         # transfer settings to local namespace
-        num_cells = settings['num_cells']
-        cell_freq_distro = settings['cell_freq_distro']
-        cell_freq_constant = settings['cell_freq_constant']
+        num_cells = self.settings['num_cells']
+        cell_freq_distro = self.settings['cell_freq_distro']
+        cell_freq_constant = self.settings['cell_freq_constant']
 
         # create local cell IDs
-        self.cells = [((i,),(i,)) for i in xrange(len(num_cells))]
+        self.cells = [((i,),(i,)) for i in xrange(num_cells)]
         np.random.shuffle(self.cells)
 
         # create frequencies associations
         if cell_freq_distro == 'constant':
             self.freqs = [1./num_cells for _ in xrange(num_cells)]
         elif cell_freq_distro == 'power-law':
-            self.freqs = 10.**(-cell_freq_constant*np.log10(np.arange(1,n+1)))
+            self.freqs = 10.**(-cell_freq_constant*np.log10(np.arange(1,num_cells+1)))
             self.freqs = self.freqs/sum(self.freqs)
 
     def generate_data(self):
@@ -88,30 +93,58 @@ class DataGenerator(object):
         """ Generate data from user settings """
 
         # transfer settings to local namespace
-        num_wells = settings['num_wells']
-        cpw = settings['cpw']
-        num_wells = settings['num_wells']
-        cpw_distro = settings['cpw_distro']
+        num_cells = self.settings['num_cells']
+        num_wells = self.settings['num_wells']
+        cpw = self.settings['cpw']
+        chain_deletion_prob = self.settings['chain_deletion_prob']
 
         # check if cells have already been generated
         if self.cells == None:
             self.generate_cells()
 
         # interpret user input for cpw distribution
-        if cpw_distro == 'constant':
-            assert isinstance(cpw,(int,float)),"cpw must be <int> or <float>"
+        if isinstance(cpw,int) and isinstance(num_wells,int):
             self.cpw = [cpw for _ in xrange(num_wells)]
-        elif cpw_distro == 'explicit':
-            assert isinstance(cpw,dict),"cpw must be <list> or <tuple>"
-            assert all([k in cpw.keys() for k in ['partitions','cpw']]),"correct keys not present"
-            
+        if isinstance(cpw,(tuple,list)) and isinstance(num_wells,(tuple,list)):
+            assert len(cpw) == len(num_wells),"cpw and num_wells not same length"
+            self.cpw = [c for w,c in zip(num_wells,cpw) for _ in xrange(w)]
 
-        
+        # start generating well data
+        self.well_data = {'A':[],'B':[]}
+        for well_ind,cpw in enumerate(self.cpw):
+            # select cell indices for each well
+            indices = np.random.choice(num_cells,size=(cpw,),p=self.freqs)
+            # seperate chains in to a,b
+            a = list(itertools.chain(*[self.cells[i][0] for i in indices]))
+            b = list(itertools.chain(*[self.cells[i][1] for i in indices]))
+            # shuffle listed chains
+            np.random.shuffle(a)
+            np.random.shuffle(b)
+            # truncation length, then remove duplicates
+            a_ind,b_ind = binomial(len(a),chain_deletion_prob),binomial(len(b),chain_deletion_prob)
+            a,b = set(a[a_ind:]),set(b[b_ind:])
+            # store in wells
+            self.well_data['A'].append(a)
+            self.well_data['B'].append(b)
+
+        # compile useful information
+        data = {
+                'well_data':self.well_data,
+                'cells':dict([(c,f) for c,f in zip(self.cells,self.freqs)]),
+                'settings':self.settings
+               }
+
+        return data # return results
 
 #------------------------------------------------------------------------------# 
+""" Factory Methods """
+#------------------------------------------------------------------------------# 
 
+#------------------------------------------------------------------------------# 
+""" Namespace Catch """
+#------------------------------------------------------------------------------# 
 if __name__ == "__main__":
     sg = DataGenerator()
-    sh
-
+    sg.generate_cells()
+    sg.generate_data()
 
