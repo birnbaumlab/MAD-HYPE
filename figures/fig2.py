382760b166c610194c92d5892a4b67b89f2de2c5
Patrick Holec <pholec@mit.edu>
2017-12-22 17:45:02 -0500

    minor modifications to figure generation

--- figures/fig2.py
+++ figures/fig2.py
@@ -7,6 +7,7 @@ Makes the set of preliminary figures
 
 # nonstandard libraries
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
@@ -18,51 +19,85 @@ plt.rcParams["font.family"] = "FreeSerif"
 
 
 def main(*args,**kwargs):
+
+    settings = {
+                'subplots':[2]
+               }
+
+    # update settings
+    for arg in args: settings.update(arg)
+    settings.update(kwargs)
+
     """ Match probability as a function of w_a,w_b """
 
-    w_i_range = (0,24)
-    w_j_range = (0,24)
-    prior = 1e-5
-    X, Y = np.mgrid[w_i_range[0]:w_i_range[1]+1,w_j_range[0]:w_j_range[1]+1]
+    if 1 in settings['subplots']:
+        # TODO: Figure out better way to pass arguments
+        """ Subplot 1 """
 
-    Z = np.zeros((X.shape[0],Y.shape[1]))
+        # Spanning parameters
+        w_i_range = (0,24)
+        w_j_range = (0,24)
+        prior = 1e-5
+        
+        # Create match built from parameters
+        X, Y = np.mgrid[w_i_range[0]:w_i_range[1]+1,w_j_range[0]:w_j_range[1]+1]
 
-    well_data = {
-                 'w_ij':(24,),
-                 'w_o':(48,),
-                 'w_tot':(96,),
-                 'cpw':(10,),
-                 'alpha':1
-                }
+        # Preallocate data matrix space
+        Z = np.zeros((X.shape[0],Y.shape[1]))
 
-    for i in xrange(X.shape[0]):
-        for j in xrange(Y.shape[1]):
-            well_data['w_i'] = (X[i,j],)
-            well_data['w_j'] = (Y[i,j],)
-            val = match_probability(well_data,0.01)[0]
-            Z[i,j] = 1./(1.+prior*val)
+        # Define default well distribution parameters
+        well_data = {
+                     'w_ij':(24,),
+                     'w_o':(48,),
+                     'w_tot':(96,),
+                     'cpw':(10,),
+                     'alpha':1
+                    }
 
+        for i in xrange(X.shape[0]):
+            for j in xrange(Y.shape[1]):
+                well_data['w_i'] = (X[i,j],)
+                well_data['w_j'] = (Y[i,j],)
+                val = match_probability(well_data,0.01)[0]
+                Z[i,j] = 1./(1.+prior*val)
 
-    fig, ax = plt.subplots(1,1)
+        matplotlib.rcParams['xtick.labelsize'] = 16
+        matplotlib.rcParams['ytick.labelsize'] = 16
 
-    #cax = ax.imshow(data,interpolation='nearest')
-    #ax.set_aspect(aspect='auto')
+        fig, ax = plt.subplots(1,1)
 
-    pcm = ax.pcolor(X, Y, Z,
-                   #norm=colors.LogNorm(vmin=1e-5, vmax=1),
-                   norm=colors.Normalize(vmin=0, vmax=1),
-                   cmap='PuBu_r')
-    cbar = fig.colorbar(pcm, ax=ax)
-    cbar.ax.tick_params(labelsize=16)
-    cbar.set_ticks([0, .5, 1])
-    cbar.set_ticklabels(['0%', '50%', '100%'])
+        #cax = ax.imshow(data,interpolation='nearest')
+        #ax.set_aspect(aspect='auto')
 
-    #ax.set_yticks(c_inds)
-    #ax.set_yticklabels(c_labels)
+        pcm = ax.pcolor(X, Y, Z,
+                       #norm=colors.LogNorm(vmin=1e-5, vmax=1),
+                       norm=colors.Normalize(vmin=0, vmax=1),
+                       cmap='PuBu_r')
+        cbar = fig.colorbar(pcm, ax=ax)
+        cbar.ax.tick_params(labelsize=16)
+        cbar.set_ticks([0, .5, 1])
+        cbar.set_ticklabels(['0%', '50%', '100%'])
 
-    plt.xlabel('$w_{i}$',fontsize=16)
-    plt.ylabel('$w_{j}$',fontsize=16)
-    #cbar.ax.set_yticklabels(['0%','100%'])  # vertically oriented colorbar
+        #ax.set_yticks(c_inds)
+        #ax.set_yticklabels(c_labels)
+
+        plt.xlabel('$w_{i}$',fontsize=20)
+        plt.ylabel('$w_{j}$',fontsize=20)
+        #cbar.ax.set_yticklabels(['0%','100%'])  # vertically oriented colorbar
+
+    if 2 in settings['subplots']:
+        #TODO: add functionality
+        """ Subplot 2 """
+
+    if 3 in settings['subplots']:
+        #TODO: add functionality
+        """ Subplot 3 """
+        pass
+
+    if 4 in settings['subplots']:
+        #TODO: add functionality
+        """ Subplot 4 """
+        pass
 
     plt.show(block=False)
     raw_input('Press enter to close...')
