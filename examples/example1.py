4bb6b49a7889fbec1e93faf8888b7231c26661dc
Joseph Berleant <jberleant@gmail.com>
2018-06-25 16:53:40 +0000

    small fixes to example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -5,10 +5,10 @@ import madhype
 
 # Set up run parameters
 solvers = ['madhype', 'alphabetr']
-solver_options = [{}, {}] # don't change default parameters
+solver_options = [{}, {'pair_threshold':-1}] # don't change default parameters
 
 # Run MAD-HYPE with default parameters
-results = madhype.simulate_run(solvers, solver_options)
+seq_data, results = madhype.simulate_run(solvers, solver_options, alpha_sharing_probs=None, beta_sharing_probs=None, visual=False)
 
 # Print out results
 for solver, result in zip(solvers, results):
